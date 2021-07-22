7cc80d3e9aba86022e867e05788b7243d0b6aad0
Satyajit Mahunta <satyajitmahunta98@gmail.com>
2021-07-22 13:50:27 +0530

    Dictionary.json file location bug resolved

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -7,7 +7,7 @@ class DictionaryController < ApplicationController
     # POST /products or /products.json
     def create
         if Dictionary.first == nil
-            @@dic_data = File.read("C:\\Users\\91754\\Desktop\\Ruby_On_Rails_Projects\\word_dict\\public\\dictionary.json")
+            @@dic_data = File.read("C:\\Users\\91754\\Desktop\\Ruby_On_Rails_Projects\\word_dict_api\\public\\dictionary.json")
             response = JSON.parse(@@dic_data)
             response.each { |key, value| Dictionary.create!(word: key, data: value) }
         end
