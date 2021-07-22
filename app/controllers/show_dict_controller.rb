d2f71cd3a27dba5934b29df4961b1ad6c34c0021
Satyajit Mahunta <satyajitmahunta98@gmail.com>
2021-07-22 17:47:32 +0530

    API Validation Added

--- app/controllers/show_dict_controller.rb
+++ app/controllers/show_dict_controller.rb
@@ -1,11 +1,16 @@
 class ShowDictController < ApplicationController
    
     def show
-        @dog = params[:word]
-        @cat = params[:type]
-        
-        @lion = Dictionary.find_by(word: @dog)
-        render :json => @lion["data"][@cat]
+        @word_name = params[:word]
+        @operation_type = params[:type]
+        @api_key = params[:API_KEY]
+        # Check API Key is Present or not
+        if Api.exists?(apikey: @api_key)
+            @data = Dictionary.find_by(word: @word_name)
+            render :json => @data["data"][@operation_type]
+        else
+            render :json => {status: "WRONG API KEY :( " }
+        end        
     end 
-
+    
 end    
